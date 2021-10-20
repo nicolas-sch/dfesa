a61e2e687cfc3cac14c7e4b26da7e9382f5221f3
nicolas-sch <nicolasdrivebackup@gmail.com>
2021-10-20 15:03:31 -0300

    FAQ position fixed

--- src/components/faqPerguntas.js
+++ src/components/faqPerguntas.js
@@ -1,31 +1,3 @@
-// import React from 'react';
-// import '../styles/scss/main.css';
-// import './Accordion.css';
-// import Accord from './Accordion';
-
-// export class FaqPerguntas extends React.Component {
-
-//   render() {
-//     return (
-//       <div>
-        // <section className="faq_perguntas">
-        //     <div className="faq_perguntas_container">
-        //         <div className="faq_perguntas_title">
-        //             <h3>
-        //                 Perguntas frequentes
-        //             </h3>
-        //         </div>
-                
-        //     </div>
-        // </section>
-
-//       </div>
-//     );
-//   }
-// }
-
-// export default FaqPerguntas;
-
 import React, { Fragment } from "react";
 import Faq from "react-faq-component";
 import data from "./data";

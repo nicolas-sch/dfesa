e21643b405f3d33c2fbff1843a9ecf4608c8d15e
nicolas-sch <nicolasdrivebackup@gmail.com>
2021-10-26 16:30:27 -0300

    faq questions inserted

--- src/components/data.js
+++ src/components/data.js
@@ -1,18 +1,18 @@
 const data = {
     rows: [
       {
-        title: "Em caso de emergência, como devo proceder?",
-        content: "Lorem ipsum dolor sit amet, consectetur "
+        title: "É permitido a pesca na região?",
+        content: "Sim, é permitido. Os estudos de ictiofauna que realizamos desde 1998 no Rio Jacuí quantificaram os peixes nos ambientes influenciados pela Usina de Dona Francisca. São 80 espécies, 76 nativas e 4 exóticas, distribuídas em 19 famílias. Durante o período de reprodução dos peixes (Piracema), há restrições. Entre as principais recomendações para a Piracema, estão: Não realizar pesca com redes de quaisquer tamanhos; Não realizar pesca com espinhéis (linha com vários anzóis); Só será permitida a pesca artesanal com caniço de bambu ou carretilha."
       },
       {
         title: "Venda de energia",
         content:
-          "Nunc maximus, magna at ultricies elementum, risus turpis vulputate quam. Und was ist, wenn der Satz länger ist ...? Geht es dann einfach weiter?"
+          "Os quase 700.000MWh de energia que produzimos por ano são destinados inteiramente aos sócios da DFESA, de acordo com as suas proporcionalidades no negócio."
       },
       {
-        title: "É permitido a pesca na região?",
+        title: "Em caso de emergência, como devo proceder?",
         content:
-          "Curabitur laoreet, mauris vel blandit fringilla, leo elit rhoncus nunc"
+          "Abaixo, faça o download de todos os contatos que podem te ajudar."
       },
     ]
   };
